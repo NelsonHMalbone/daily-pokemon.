00159b61efc5177468ccdb84e9d0c031540c79c4
Nelson Malbone <nhmalbone@icloud.com>
2026-01-17 01:03:05 -0500

    just more less messing around with the code and the debugger at the time

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 import requests
-import json
 import random
 
 
 url = 'https://pokeapi.co/api/v2/pokemon?limit=100000'
 request = requests.get(url)
-request.raise_for_status()
 content = request.json()
+print(content['results']['name'])
 
-pokemon_data = content["results"]
 
-random_pokemon = random.choice(pokemon_data)
+#pokemon_data = content["results"]
 
-print("Random Pokemon")
-print("name: ", random_pokemon["name"])
-print("url: ", random_pokemon["url"])
+#random_pokemon = random.choice(pokemon_data)

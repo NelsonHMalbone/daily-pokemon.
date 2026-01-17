4042864813c504d3d3bfe388e9304ca21b5d9f8e
Nelson Malbone <nhmalbone@icloud.com>
2026-01-17 14:10:04 -0500

    working with the news api instead of the pokemon api. created a .env file to hide my api code

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import requests
-import random
+from dotenv import load_dotenv
+import os
+from pathlib import Path
 
 
-url = 'https://pokeapi.co/api/v2/pokemon?limit=100000'
-request = requests.get(url)
-content = request.json()
-print(content['results']['name'])
+
+# loading dotenv file.
+env_path = Path(__file__).parent / '.env'
+load_dotenv(dotenv_path=env_path) #reads .env file
+
+API_KEY = os.getenv("MYAPIKEY")
+print(API_KEY)
 
 
-#pokemon_data = content["results"]
+if not API_KEY:
+    raise RuntimeError("API_KEY not set")
 
-#random_pokemon = random.choice(pokemon_data)
+url = f'https://newsapi.org/v2/everything?q=tesla&from=2025-12-17&sortBy=publishedAt&apiKey={API_KEY}'
+
+print(url)
+
+
+

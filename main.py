54cd0ae81fe149f4692c6e88f8ac7c149f065d7a
Nelson Malbone <nhmalbone@icloud.com>
2026-01-18 10:41:08 -0500

    added some tips and sound notes added user input for topics shows the index and article title

--- main.py
+++ main.py
@@ -3,22 +3,24 @@ from dotenv import load_dotenv
 import os
 from pathlib import Path
 
-
-
 # loading dotenv file.
 env_path = Path(__file__).parent / '.env'
 load_dotenv(dotenv_path=env_path) #reads .env file
 
 API_KEY = os.getenv("MYAPIKEY")
-print(API_KEY)
 
-
+# hard stop if api is missing
 if not API_KEY:
     raise RuntimeError("API_KEY not set")
 
-url = f'https://newsapi.org/v2/everything?q=tesla&from=2025-12-17&sortBy=publishedAt&apiKey={API_KEY}'
+user_input = input("Enter topic to get news: ")
+url = f'https://newsapi.org/v2/everything?q={user_input}&from=2025-12-17&sortBy=publishedAt&apiKey={API_KEY}'
 
-print(url)
 
+req = requests.get(url)
+content = req.json()
+
+for index, article in enumerate(content['articles'], start=1):
+    print(f'{index}: {article['title']}')
 
 

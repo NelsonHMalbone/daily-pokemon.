2deea748ba21f392b31e5e1b8f037b7e2cc5ea2e
Nelson Malbone <nhmalbone@icloud.com>
2026-01-21 13:31:20 -0500

    Added a read me file to the project

--- main.py
+++ main.py
@@ -13,15 +13,17 @@ API_KEY = os.getenv("MYAPIKEY")
 if not API_KEY:
     raise RuntimeError("API_KEY not set")
 
-user_input = input("Enter topic to get news: ")
-user_input_date = input("Enter date to get news ex 2025-12-20: ")
-url = f'https://newsapi.org/v2/everything?q={user_input}&from={user_input_date}&sortBy=publishedAt&apiKey={API_KEY}'
+#user_input = input("Enter topic to get news: ")
+#user_input_date = input("Enter date to get news ex 2025-12-20: ")
+url = f'https://newsapi.org/v2/everything?q=Code&sortBy=publishedAt&apiKey={API_KEY}'
 
 
 req = requests.get(url)
 content = req.json()
 
 for index, article in enumerate(content['articles'], start=1):
-    print(f'{index}: {article['title']} \n {article["description"]} \n {article["url"]} \n')
+    print(f'{index}: {article['title']} \n '
+          f'{article["description"]} \n '
+          f'{article["url"]} \n')
 
 
---
+++ send_email.py
@@ -0,0 +1,30 @@
+import smtplib, ssl, certifi
+from email.message import EmailMessage
+from dotenv import load_dotenv
+import os
+from pathlib import Path
+
+env_path = Path(__file__).parent / '.env'
+load_dotenv(dotenv_path=env_path)
+
+def send_email(message): # this is a string
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = os.getenv("USERNAME")
+    password = os.getenv("APPPASS")
+    recipients = os.getenv("RECIPIENTS")
+
+    msg = EmailMessage()
+    msg["From"] = username
+    msg["To"] = recipients
+    msg["Subject"] = "Test Email"
+    msg.set_content(message)
+
+    context = ssl.create_default_context(cafile=certifi.where())
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.send_message(msg)
+
+send_email("Hello World")
